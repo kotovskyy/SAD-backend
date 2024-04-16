b8dfb94c6837a9110295d219c67667cc4909a2aa
vizels <mirchuk.georgiy@gmail.com>
2024-04-17 01:03:16 +0200

    Apps created and added to INSTALLED_APPS

--- SAD_backend/SAD_backend/settings.py
+++ SAD_backend/SAD_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'front_communication',
+    'device_communication',
 ]
 
 MIDDLEWARE = [
