f0515558042be297c1ec5a0e3023e3afebd14c5e
Vizels <mirchuk.georgiy@gmail.com>
2024-04-22 19:36:39 +0200

    SAD-16 <Fixed DB rows names according to Django standards.>

--- SAD_backend/device_communication/models.py
+++ SAD_backend/device_communication/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Device(models.Model):
     device_name = models.CharField(max_length=100)
     mac_address = models.CharField(max_length=100)
-    user_id = models.ForeignKey('front_communication.User', on_delete=models.CASCADE)
+    user = models.ForeignKey('front_communication.User', on_delete=models.CASCADE)
     sleep_time = models.TimeField(default='22:00:00')
     wake_time = models.TimeField(default='06:00:00')
     preferred_temperature = models.FloatField(default=19.0)
 
 class Measurement(models.Model):
-    device_id = models.ForeignKey(Device, on_delete=models.CASCADE)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     temperature = models.FloatField()
     humidity = models.FloatField()
