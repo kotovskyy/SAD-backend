d45d7e9c1f1532d86310e170a685a53fdd456039
acodered <alexkondratru@gmail.com>
2024-05-03 20:02:03 +0200

    Add initial viewsets

--- SAD_backend/API/views.py
+++ SAD_backend/API/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from device_communication.models import Device, Measurement
+from front_communication.models import User
+from API.serializers import DeviceSerializer, MeasurementSerializer, UserSerializer
 
-# Create your views here.
+
+class DeviceViewSet(viewsets.ModelViewSet):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+
+class MeasurementViewSet(viewsets.ModelViewSet):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
