d42d78e523368254f699fd52fd733a19d413e345
Vizels <mirchuk.georgiy@gmail.com>
2024-06-02 01:07:13 +0200

    Models edited for better scalability

--- SAD_backend/device_communication/models.py
+++ SAD_backend/device_communication/models.py
@@ -1,17 +1,65 @@
 from django.db import models
+from django.utils import timezone
+
+class Device_type(models.Model):
+    id = models.SmallAutoField(primary_key=True)
+    device_type = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'device_type'
 
 # Create your models here.
 class Device(models.Model):
-    device_name = models.CharField(max_length=100)
-    mac_address = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     user = models.ForeignKey(
         'front_communication.User',
         related_name='devices',
         on_delete=models.CASCADE
     )
-    sleep_time = models.TimeField(default='22:00:00')
-    wake_time = models.TimeField(default='06:00:00')
-    preferred_temperature = models.FloatField(default=19.0)
+    type = models.ForeignKey(
+        Device_type,
+        related_name='devices',
+        on_delete=models.CASCADE,
+        null=True
+    )
+    mac_address = models.CharField(max_length=17)
+
+    class Meta:
+        db_table = 'device'
+
+class Setting_type(models.Model):
+    id = models.SmallAutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    unit = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'setting_type'
+
+class Setting(models.Model):
+    device = models.ForeignKey(
+        Device,
+        related_name='settings',
+        on_delete=models.CASCADE
+    )
+    type = models.ForeignKey(
+        Setting_type,
+        related_name='settings',
+        on_delete=models.CASCADE,
+        null=True
+    )
+    value = models.IntegerField()
+
+    class Meta:
+        db_table = 'setting'
+    
+
+class Measurement_type(models.Model):
+    id = models.SmallAutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    unit = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'measurement_type'
 
 class Measurement(models.Model):
     device = models.ForeignKey(
@@ -19,7 +67,14 @@ class Measurement(models.Model):
         related_name='measurements',    
         on_delete=models.CASCADE
     )
-    timestamp = models.DateTimeField(auto_now_add=True)
-    temperature = models.FloatField()
-    humidity = models.FloatField()
-    light_intensity = models.IntegerField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    type = models.ForeignKey(
+        Measurement_type,
+        related_name='measurements',
+        on_delete=models.CASCADE,
+        null=True
+    )
+    value = models.IntegerField()
+
+    class Meta:
+        db_table = 'measurement'
--- SAD_backend/front_communication/models.py
+++ SAD_backend/front_communication/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    class Meta:
+        db_table = 'user'
