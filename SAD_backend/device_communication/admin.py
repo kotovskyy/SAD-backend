110c627e62ecb3ae2f95bad8ae87fca5788a5f81
Vizels <mirchuk.georgiy@gmail.com>
2024-06-16 18:59:01 +0200

    Show the names of devices and types

--- SAD_backend/device_communication/admin.py
+++ SAD_backend/device_communication/admin.py
@@ -6,19 +6,39 @@ class DeviceTypeAdmin(admin.ModelAdmin):
     list_display = ('id', 'device_type')
 
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'user', 'type', 'mac_address')
+    list_display = ('id', 'name', 'user', 'device_type_display', 'mac_address')
+
+    @admin.display(description='Type')
+    def device_type_display(self, obj):
+        return obj.type.id, obj.type.device_type
 
 class SettingTypeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'unit')
 
 class SettingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'device', 'type', 'value')
+    list_display = ('id', 'device_display', 'type_display', 'value')
+
+    @admin.display(description='Device')
+    def device_display(self, obj):
+        return obj.device.name
+    
+    @admin.display(description='Type')
+    def type_display(self, obj):
+        return obj.type.id, obj.type.name
 
 class MeasurementTypeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'unit')
 
 class MeasurementAdmin(admin.ModelAdmin):
-    list_display = ('id', 'device', 'timestamp', 'type', 'value')
+    list_display = ('id', 'device_display', 'timestamp', 'type_display', 'value')
+
+    @admin.display(description='Device')
+    def device_display(self, obj):
+        return obj.device.name
+    
+    @admin.display(description='Type')
+    def type_display(self, obj):
+        return obj.type.id, obj.type.name
 
 # Register each model with its custom ModelAdmin class
 admin.site.register(Device_type, DeviceTypeAdmin)
