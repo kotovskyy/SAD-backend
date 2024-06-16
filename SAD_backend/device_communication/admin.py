b3704f9e46203d4a80337860382b75acadc0a7e7
Vizels <mirchuk.georgiy@gmail.com>
2024-06-16 18:39:13 +0200

    Admin tables show every field now

--- SAD_backend/device_communication/admin.py
+++ SAD_backend/device_communication/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 from device_communication.models import Device, Measurement, Device_type, Setting_type, Setting, Measurement_type
 
-# Register your models here.
-admin.site.register([Device, Measurement, Device_type, Setting_type, Setting, Measurement_type])
+# Custom ModelAdmin classes for each model
+class DeviceTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'device_type')
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'user', 'type', 'mac_address')
+
+class SettingTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'unit')
+
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'device', 'type', 'value')
+
+class MeasurementTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'unit')
+
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'device', 'timestamp', 'type', 'value')
+
+# Register each model with its custom ModelAdmin class
+admin.site.register(Device_type, DeviceTypeAdmin)
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(Setting_type, SettingTypeAdmin)
+admin.site.register(Setting, SettingAdmin)
+admin.site.register(Measurement_type, MeasurementTypeAdmin)
+admin.site.register(Measurement, MeasurementAdmin)
